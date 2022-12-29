566431b4bc4e6d9e9d208a70794bedb807e2a39c
willwula <wfh318@gmail.com>
2022-12-29 18:51:43 +0800

    ep5-3:create router.php

--- index.php
+++ index.php
@@ -1,27 +1,4 @@
 <?php
 require 'function.php';
 
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-
-//if ($uri === '/'){
-//    require 'controllers/index.php';
-//}elseif ($uri === '/about'){
-//    require 'controllers/about.php';
-//}elseif ($uri ==='/contact'){
-//    require 'controllers/contact.php';
-//}
-
-$routes = [
-    '/' => 'controllers/index.php',
-    '/about' => 'controllers/about.php',
-    '/contact' => 'controllers/contact.php',
-];
-
-if (array_key_exists($uri,$routes)) {
-    require $routes[$uri];
-}else{
-    http_response_code(404);
-    require 'views/404.php';
-    die();
-}
+require 'router.php';
