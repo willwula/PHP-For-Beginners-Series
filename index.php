210b9523097876e7a0ffd86c2545d2391b05cb0d
willwula <wfh318@gmail.com>
2022-12-29 17:38:57 +0800

    ep5-2

--- index.php
+++ index.php
@@ -1,12 +1,27 @@
 <?php
 require 'function.php';
 
-$uri = $_SERVER['REQUEST_URI'];
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
-if ($uri === '/'){
-    require 'controllers/index.php';
-}elseif ($uri === '/about'){
-    require 'controllers/about.php';
-}elseif ($uri ==='/contact'){
-    require 'controllers/contact.php';
+
+//if ($uri === '/'){
+//    require 'controllers/index.php';
+//}elseif ($uri === '/about'){
+//    require 'controllers/about.php';
+//}elseif ($uri ==='/contact'){
+//    require 'controllers/contact.php';
+//}
+
+$routes = [
+    '/' => 'controllers/index.php',
+    '/about' => 'controllers/about.php',
+    '/contact' => 'controllers/contact.php',
+];
+
+if (array_key_exists($uri,$routes)) {
+    require $routes[$uri];
+}else{
+    http_response_code(404);
+    require 'views/404.php';
+    die();
 }
