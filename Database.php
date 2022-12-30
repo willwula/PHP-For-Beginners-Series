c0908b14fd5d32da4624a39a879a7854cb36f46c
willwula <wfh318@gmail.com>
2022-12-30 17:36:43 +0800

    ep9:create config.php

--- Database.php
+++ Database.php
@@ -2,9 +2,14 @@
 class Database{
 //    private $dsn;
     private $connection;
-    public function __construct(){
-        $dsn = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4";
-        $this->connection = new PDO($dsn,"root","hell");
+    public function __construct($config, $username = 'root',$password = 'hell'){
+//        $dsn = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4";
+        $dsn = http_build_query($config,'',';');
+        $dsn = "mysql:$dsn";
+//        dd($dsn);
+        $this->connection = new PDO($dsn,$username,$password, [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
     }
     public function query($query){
 
--- index.php
+++ index.php
@@ -2,8 +2,11 @@
 require 'function.php';
 require 'Database.php';
 
-$db = new Database();
-$posts = $db->query("select * from posts ")->fetchAll(PDO::FETCH_ASSOC);;
+$config = require 'config.php';
+
+$db = new Database($config['database']);
+
+$posts = $db->query("select * from posts ")->fetchAll();
 
 dd($posts);
 //foreach ($posts as $post ){
